295a66f64843ebc081e2b5f67a432154a33f8333
Myself <Myself@LAPTOP-I71AGAU1>
2023-07-13 17:34:40 +0530

    10

--- ActiTime/src/test/java/com/actiTime/testcase/DemoTest.java
+++ ActiTime/src/test/java/com/actiTime/testcase/DemoTest.java
@@ -2,11 +2,12 @@ package com.actiTime.testcase;
 
 
 
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import com.genricUtility.BaseClass;
 
-
+@Listeners(com.genricUtility.ListenerImplementation1.class)
 public class DemoTest extends BaseClass{
 	
 	@Test
